16b805bcee0bf12b7dc6a838a6b7dc1f1a76712a
temur01 <suvonovtemur2008@gmail.com>
2025-10-17 01:01:29 +0500

    Test categories route with alternative path /api/cats

--- server/src/server.ts
+++ server/src/server.ts
@@ -97,6 +97,7 @@ app.use((req, res, next) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/meals', mealsRoutes);
 app.use('/api/categories', categoriesRoutes);
+app.use('/api/cats', categoriesRoutes); // Test with different path
 
 // Test endpoint to verify routing
 app.get('/api/test-categories', (req, res) => {
